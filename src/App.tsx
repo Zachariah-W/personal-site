04391afe7e293fd25aef100612aec928b33ab191
Zachariah-W <zachwangs@gmail.com>
2025-01-07 16:49:17 -0800

    Fix flip animation

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="w-screen h-screen bg-neutral-900 flex justify-center items-center">
-      <div className="shadow-xl bg-rain bg-neutral-800 rounded-2xl text-neutral-300 border border-neutral-700">
-        <Routes>
-          <Route path="/" element={<BasicInfo />} />
-        </Routes>
-      </div>
+    <div
+      className="w-screen h-screen bg-neutral-900 flex justify-center items-center"
+      style={{ perspective: "1000px" }}
+    >
+      <Routes>
+        <Route path="/" element={<BasicInfo />} />
+      </Routes>
     </div>
   );
 }
--- src/BasicInfo.tsx
+++ src/BasicInfo.tsx
@@ -1,9 +1,9 @@
-import ReactCardFlip from "react-card-flip";
 import { useState } from "react";
 import Projects from "./Projects";
 import AboutMe from "./AboutMe";
 import Photoshop from "./Photoshop";
 import DesignAnimation from "./DesignAnimation";
+import { motion } from "framer-motion";
 
 type PageStatus = {
   status: "home" | "about" | "design" | "photo" | "project";
@@ -11,22 +11,42 @@ type PageStatus = {
 
 const BasicInfo = () => {
   const [flip, setFlip] = useState<boolean>(false);
+  const [animating, setAnimating] = useState<boolean>(false);
   const [page, setPage] = useState<PageStatus>({ status: "home" });
+
   const flipCard = (page: PageStatus) => {
-    setPage(page);
-    setFlip(!flip);
+    if (!animating) {
+      setFlip(!flip);
+      setAnimating(true);
+      setPage(page);
+    }
   };
 
   return (
-    <ReactCardFlip flipDirection="horizontal" isFlipped={flip}>
-      <div className="flex flex-row items-center justify-evenly gap-6 py-10 px-10 card">
+    <motion.div
+      className="flex justify-center items-center *:w-full *:h-full"
+      initial={false}
+      style={{
+        transformStyle: "preserve-3d",
+      }}
+      animate={
+        flip
+          ? { rotateY: 180, height: "100vh", width: "100vw" }
+          : { rotateY: 360, height: "260px", width: "480px" }
+      }
+      transition={{ duration: 0.6 }}
+      onAnimationComplete={() => {
+        setAnimating(false);
+      }}
+    >
+      <div className="backface-hidden absolute flex flex-row items-center justify-evenly gap-6 px-10 py-8 bg-rain shadow-xl bg-neutral-800 rounded-2xl text-neutral-300 border border-neutral-800">
         <div className="flex flex-col justify-center gap-3 items-center h-full">
           <img
             className="rounded-2xl max-w-32"
             src="/src/assets/Black_Linear_Fill_Black_Bg.png"
             alt="Image of Zachariah Wang"
           />
-          <div className="flex flex-row items-center justify-center gap-3 *:max-w-8">
+          <div className="flex flex-row items-center justify-center gap-3 *:w-8">
             <a href="mailto:zachwangs@gmail.com">
               <img
                 className="rounded-full hover:scale-105 duration-200"
@@ -92,13 +112,15 @@ const BasicInfo = () => {
           </button>
         </div>
       </div>
-      <div className="card card-back max-w-screen-lg p-2">
-        {page.status == "about" && <AboutMe />}
-        {page.status == "project" && <Projects />}
-        {page.status == "photo" && <Photoshop />}
-        {page.status == "design" && <DesignAnimation />}
+      <div className="backface-hidden absolute flip-card-back bg-transparent p-3 sm:p-14 text-neutral-300 flex justify-center items-center">
+        <div className="bg-rain rounded-2xl border border-neutral-800 shadow-xl p-5 overflow-y-auto max-h-[630px]">
+          {page.status == "about" && <AboutMe />}
+          {page.status == "project" && <Projects />}
+          {page.status == "photo" && <Photoshop />}
+          {page.status == "design" && <DesignAnimation />}
+        </div>
       </div>
-    </ReactCardFlip>
+    </motion.div>
   );
 };
 
